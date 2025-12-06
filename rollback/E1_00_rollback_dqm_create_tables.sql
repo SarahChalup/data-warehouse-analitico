aa4cc898f7d1013387438dc141516504fc3cd449
SarahChalup <saritachalup@gmail.com>
2025-12-05 21:15:48 -0300

    Agregando export dump de base de datos

--- rollback/E1_00_rollback_dqm_create_tables.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS dqm_table_profile;
-DROP TABLE IF EXISTS dqm_field_checks;
-DROP TABLE IF EXISTS dqm_object_inventory;
-DROP TABLE IF EXISTS dqm_exec_log;
-DROP TABLE IF EXISTS dqm_scripts_inventory;
